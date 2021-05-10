115def325eeba2e50fe79a60480876e1d79eda33
colinclerk <colin@clerk.dev>
2021-05-10 00:37:53 -0400

    Add frontend to request api

--- app/javascript/packs/main.jsx
+++ app/javascript/packs/main.jsx
@@ -2,25 +2,48 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const APIRequestButton = () => {
+  const [error, setError] = React.useState(null);
+  const [response, setResponse] = React.useState(null);
+  const makeRequest = async () => {
+    const response = await fetch("/api/clerk_session");
+    if (response.status == 200) {
+      const session = await response.json();
+      setResponse(session);
+    } else {
+      setError(`Server failed with ${response.status}`);
+    }
+  };
 
-Hello.defaultProps = {
-  name: 'David'
-}
+  return (
+    <div style={{ textAlign: "center" }}>
+      <button onClick={makeRequest}>Make request</button>
+      {response && <div style={{ width: 400 }}>Response: {response}</div>}
+      {error && <div style={{ width: 400 }}>Error: {error}</div>}
+    </div>
+  );
+};
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <ClerkProvider frontendApi="clerk.7ica9.qk1o9.lcl.dev">
+      <SignedIn>
+        <APIRequestButton />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Send all non-api routes to react
   root 'react#main'
-  get '*path' => 'react#main'
+  get '*path' => 'react#main', constraints: 
+    lambda { |request| 
+      request.path != "/api" and !request.path.start_with?("/api/")
+    }
 end
